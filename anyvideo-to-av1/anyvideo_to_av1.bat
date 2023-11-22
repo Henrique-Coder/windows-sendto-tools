6ba34da97e8f0994983ac589d23b1f6c0d6ef0a5
Henrique <henriquemoreira10fk@gmail.com>
2023-11-22 13:13:30 -0300

    `prettified` code and echo's

--- anyvideo-to-av1/anyvideo_to_av1.bat
+++ anyvideo-to-av1/anyvideo_to_av1.bat
@@ -8,7 +8,7 @@ set "output_filepath=%~dp1%~n1-libsvtav1.mp4"
 
 REM Checks if the input file exists
 if not exist "%input_file_path%" (
-    echo. && echo [error] Input file does not exist: "%input_file_path%"
+    echo. && echo [error] Input file does not exist. Try again with a valid file.
     goto endapp
 )
 
@@ -24,14 +24,13 @@ if errorlevel 1 (
     goto endapp
 )
 
-REM Get audio codec and bitrate from input file
+REM Get audio codec and audio bitrate
 REM ...
+set "ffmpeg_get_codec_command"="ffprobe.exe" -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "%input_file_path%"
+set "ffmpeg_get_bitrate_command"="ffprobe.exe" -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 "%input_file_path%"
 
 REM If audio codec is opus, keep the bitrate, else convert to opus with 128kbps
 REM ...
-set "audio_codec"=""
-set "audio_bitrate"=""
-
 if "%audio_codec%"=="libopus" (
     set "audio_bitrate=%audio_bitrate%"
 ) else (
@@ -39,12 +38,21 @@ if "%audio_codec%"=="libopus" (
     set "audio_bitrate=128"
 )
 
+REM Show transcoding information
+echo. && echo [info] Starting file transcoding... && echo.
+echo [info] Input file: "%input_file_path%"
+echo [info] Output file: "%output_filepath%" && echo.
+echo [info] Video codec: "libsvtav1" (new)
+echo [info] Video bitrate: (same as input)
+echo [info] Audio codec: "%audio_codec%" (new)
+echo [info] Audio bitrate: "%audio_bitrate%kbps" (new)
+echo.
+
 REM Runs transcoding using the "libsvtav1" codec for video and the determined settings for audio
-echo. && echo [running] Starting file transcoding. && echo.
 "ffmpeg.exe" -i "%input_file_path%" -c:v libsvtav1 -c:a %audio_codec% -b:a %audio_bitrate%k -y "%output_filepath%" -hide_banner -stats -loglevel warning
 if errorlevel 0 (
     echo. && echo [success] File transcoding completed: "%output_filepath%"
-    ::"nircmd.exe" moverecyclebin "%input_file_path%"
+    "nircmd.exe" moverecyclebin "%input_file_path%"
     goto endapp
 ) else (
     echo. && echo [error] File transcoding failed.
