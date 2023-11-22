fa24f674cb523397f7dd0d42372e4568561810d1
Henrique <henriquemoreira10fk@gmail.com>
2023-11-22 12:31:37 -0300

    `add` audio codec detector (beta)

--- anyvideo-to-libsvtav1/anyvideo_to_av1.bat
+++ anyvideo-to-libsvtav1/anyvideo_to_av1.bat
@@ -2,7 +2,6 @@
 chcp 65001 >nul 2>&1
 setlocal enabledelayedexpansion
 
-
 REM Creates the input and output path variables of the video file
 set "input_file_path=%~1"
 set "output_filepath=%~dp1%~n1-libsvtav1.mp4"
@@ -14,23 +13,38 @@ if not exist "%input_file_path%" (
 )
 
 REM Checks if "ffmpeg.exe" and "nircmd.exe" exist in the PATH or in the current directory
-where /q ffmpeg.exe
+where /q "ffmpeg.exe"
 if errorlevel 1 (
     echo. && echo [error] "ffmpeg.exe" does not exist in PATH or current directory
     goto endapp
 )
-where /q nircmd.exe
+where /q "nircmd.exe"
 if errorlevel 1 (
     echo. && echo [error] "nircmd.exe" does not exist in PATH or current directory
     goto endapp
 )
 
-REM Runs transcoding using the "libsvtav1" codec for video and "libopus" for audio
+REM Get audio codec and bitrate from input file
+REM ...
+
+REM If audio codec is opus, keep the bitrate, else convert to opus with 128kbps
+REM ...
+set "audio_codec"=""
+set "audio_bitrate"=""
+
+if "%audio_codec%"=="libopus" (
+    set "audio_bitrate=%audio_bitrate%"
+) else (
+    set "audio_codec=libopus"
+    set "audio_bitrate=128"
+)
+
+REM Runs transcoding using the "libsvtav1" codec for video and the determined settings for audio
 echo. && echo [running] Starting file transcoding. && echo.
-"ffmpeg.exe" -i "%input_file_path%" -c:v libsvtav1 -c:a libopus -b:a 128k -y "%output_filepath%" -hide_banner -stats -loglevel warning
+"ffmpeg.exe" -i "%input_file_path%" -c:v libsvtav1 -c:a %audio_codec% -b:a %audio_bitrate%k -y "%output_filepath%" -hide_banner -stats -loglevel warning
 if errorlevel 0 (
     echo. && echo [success] File transcoding completed: "%output_filepath%"
-    "nircmd.exe" moverecyclebin "%input_file_path%"
+    ::"nircmd.exe" moverecyclebin "%input_file_path%"
     goto endapp
 ) else (
     echo. && echo [error] File transcoding failed.
