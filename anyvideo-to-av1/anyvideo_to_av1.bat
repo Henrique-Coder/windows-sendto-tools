07b20c4a8c8791a6f9cf59b4ffdb774ee425e866
Henrique <henriquemoreira10fk@gmail.com>
2023-11-24 17:45:22 -0300

    `prettify` comment type

--- anyvideo-to-av1/anyvideo_to_av1.bat
+++ anyvideo-to-av1/anyvideo_to_av1.bat
@@ -2,20 +2,20 @@
 chcp 65001 >nul 2>&1
 setlocal EnableDelayedExpansion
 
-:: Set terminal title
+rem Set terminal title
 title Any video to AV1 - Developed at gh@Henrique-Coder/windows-sendto-tools
 
-REM Set input file path from the first argument and the default output file name prefix
+rem Set input file path from the first argument and the default output file name prefix
 set "input_file_path=%~1"
 set "default_output_filename_prefix=-libsvtav1"
 
-REM Checks if the input file exists
+rem Checks if the input file exists
 if not exist "%input_file_path%" (
     echo. && echo [error] Input file does not exist. Try again with a valid file.
     goto endapp
 )
 
-REM Checks if "ffmpeg.exe" and "nircmd.exe" exist in the PATH or in the current directory
+rem Checks if "ffmpeg.exe" and "nircmd.exe" exist in the PATH or in the current directory
 where /q "ffmpeg.exe"
 if errorlevel 1 (
     echo. && echo [error] "ffmpeg.exe" does not exist in PATH or current directory (download it from https://ffmpeg.org/download.html)
@@ -27,11 +27,11 @@ if errorlevel 1 (
     goto endapp
 )
 
-REM TODO: Get video codec and video bitrate
+rem TODO: Get video codec and video bitrate
 :: set "ffmpeg_get_codec_command"="ffprobe.exe" -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "%input_file_path%"
 :: set "ffmpeg_get_bitrate_command"="ffprobe.exe" -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 "%input_file_path%"
 
-REM TODO: If audio codec is opus, keep the bitrate, else convert to opus with 128kbps
+rem TODO: If audio codec is opus, keep the bitrate, else convert to opus with 128kbps
 :: if "%audio_codec%"=="libopus" (
 ::     set "audio_bitrate=%audio_bitrate%"
 :: ) else (
@@ -39,19 +39,19 @@ REM TODO: If audio codec is opus, keep the bitrate, else convert to opus with 12
 ::     set "audio_bitrate=128"
 :: )
 
-REM Set video codec, audio codec and audio bitrate
+rem Set video codec, audio codec and audio bitrate
 set "video_codec=libsvtav1"
 set "audio_codec=libopus"
 set "audio_bitrate=128"
 
-REM Set output file name prefix
+rem Set output file name prefix
 :output_filename_prefix_choice
 echo. && set /p "output_filename_prefix=Output filename prefix (default: "%default_output_filename_prefix%") [allowed: alphanumeric, underline, hyphen and dot]: "
 if "%output_filename_prefix%"=="" (
     set "output_filename_prefix=%default_output_filename_prefix%"
 ) && goto extension_choice
 
-REM Set output file extension
+rem Set output file extension
 :extension_choice
 cls && echo. && echo Output filename prefix (default: "%default_output_filename_prefix%") [allowed: alphanumeric, underline, hyphen and dot]: %output_filename_prefix%
 set /p "output_extension=Output file extension (default: "mp4") [allowed: "mp4", "mkv", "webm"]: "
@@ -63,10 +63,10 @@ if not "%output_extension%"=="mp4" if not "%output_extension%"=="mkv" if not "%o
 )
 
 :start_transcoding_task
-REM Clear screen and set output file path
+rem Clear screen and set output file path
 cls && set "output_filepath=%~dp1%~n1%output_filename_prefix%.%output_extension%"
 
-REM Show transcoding information
+rem Show transcoding information
 echo. && echo [info] Starting file transcoding... && echo.
 echo [info] Input file: "%input_file_path%"
 echo [info] Output file: "%output_filepath%" && echo.
@@ -77,7 +77,7 @@ echo [info] Audio codec: "%audio_codec%"
 echo [info] Audio bitrate: "%audio_bitrate%kbps"
 echo.
 
-REM Runs transcoding using the "libsvtav1" codec for video and the determined settings for audio
+rem Runs transcoding using the "libsvtav1" codec for video and the determined settings for audio
 "ffmpeg.exe" -i "%input_file_path%" -c:v %video_codec% -c:a %audio_codec% -b:a %audio_bitrate%k -y "%output_filepath%" -hide_banner -stats -loglevel warning
 if errorlevel 0 (
     echo. && echo [success] File transcoding completed: "%output_filepath%"
