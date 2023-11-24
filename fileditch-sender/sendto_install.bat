519794a08dd16d8cde81cf01fe25c04b1a6d5dcc
Henrique <henriquemoreira10fk@gmail.com>
2023-11-24 14:18:47 -0300

    `add` Windows 10/11+ SendTo auto-installer

---
+++ fileditch-sender/sendto_install.bat
@@ -0,0 +1,34 @@
+@echo off
+chcp 65001 >nul 2>&1
+setlocal
+
+set "mainBatchScript=%~dp0fileditch_sender.bat"
+set "shortcutName=Upload the file to Fileditch.lnk"
+
+set "assetsDir=%~dp0assets"
+set "iconFile=%assetsDir%\icon.ico"
+set "sendToDir=%APPDATA%\Microsoft\Windows\SendTo"
+set "shortcutPath=%sendToDir%\%shortcutName%"
+set "targetPath=%mainBatchScript%"
+
+echo.
+if not exist "%mainBatchScript%" (
+    echo The main script was not found. Exiting...
+    goto endapp
+)
+
+if not exist "%iconFile%" (
+    echo The icon file was not found. Creating the shortcut without the icon.
+    powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%shortcutPath%'); $Shortcut.TargetPath = '%targetPath%'; $Shortcut.Save()"
+) else (
+    echo The icon file was found. Creating the shortcut with the icon.
+    powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%shortcutPath%'); $Shortcut.TargetPath = '%targetPath%'; $Shortcut.IconLocation = '%iconFile%'; $Shortcut.Save()"
+)
+
+echo Installation completed successfully. Exiting...
+goto endapp
+
+:endapp
+endlocal
+timeout /t 5 >nul 2>&1
+exit /b %errorlevel%
