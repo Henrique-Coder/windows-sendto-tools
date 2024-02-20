348ae39b6a066442d7473ad95eb63ba77c082a97
Henrique <henriquemoreira10fk@gmail.com>
2024-02-20 11:53:33 -0300

    `add` new optional input for audio bitrate and modified FFmpeg params

--- anyvideo-to-av1/anyvideo_to_av1.bat
+++ anyvideo-to-av1/anyvideo_to_av1.bat
@@ -17,34 +17,42 @@ if not exist "%input_file_path%" (
 rem Checks if "ffmpeg.exe" and "nircmd.exe" exist in the PATH or in the current directory
 where /q "ffmpeg.exe"
 if errorlevel 1 (
-    echo. && echo [error] "ffmpeg.exe" does not exist in PATH or current directory (download it from https://ffmpeg.org/download.html) && goto endapp
+    echo. && echo [error] "ffmpeg.exe" does not exist in PATH or working directory (download it from https://ffmpeg.org/download.html) && goto endapp
 )
 where /q "nircmd.exe"
 if errorlevel 1 (
-    echo. && echo [error] "nircmd.exe" does not exist in PATH or current directory (download it from https://www.nirsoft.net/utils/nircmd.html) && goto endapp
+    echo. && echo [error] "nircmd.exe" does not exist in PATH or working directory (download it from https://www.nirsoft.net/utils/nircmd.html) && goto endapp
 )
 
 rem Set video codec, audio codec and audio bitrate
 set "video_codec=libsvtav1"
 set "audio_codec=libopus"
-set "audio_bitrate=192"
+set "audio_bitrate=256"
 
 rem Set output file name prefix
 :output_filename_prefix_choice
-echo. && set /p "output_filename_prefix=Output filename prefix (default: "%default_output_filename_prefix%") [allowed: alphanumeric, underline, hyphen and dot]: "
+echo. && set /p "output_filename_prefix=Output filename prefix (default: %default_output_filename_prefix%) [allowed: alphanumeric, underline, hyphen and dot]: "
 if "%output_filename_prefix%"=="" (
     set "output_filename_prefix=%default_output_filename_prefix%" && goto extension_choice
 )
 
 rem Set output file extension
 :extension_choice
-cls && echo. && echo Output filename prefix (default: "%default_output_filename_prefix%") [allowed: alphanumeric, underline, hyphen and dot]: %output_filename_prefix%
-set /p "output_extension=Output file extension (default: "mp4") [allowed: "mp4", "mkv", "webm"]: "
+cls && echo. && echo Output filename prefix (default: %default_output_filename_prefix%) [allowed: alphanumeric, underline, hyphen and dot]: %output_filename_prefix%
+set /p "output_extension=Output file extension (default: mp4) [allowed: mp4, mkv, webm]: "
 if "%output_extension%"=="" (
-    set "output_extension=mp4" && goto start_transcoding_task
+    set "output_extension=mp4" && goto output_filename_audio_bitrate
 )
 if not "%output_extension%"=="mp4" if not "%output_extension%"=="mkv" if not "%output_extension%"=="webm" (
     goto extension_choice
+)
+
+rem Set output file audio bitrate
+:output_filename_audio_bitrate
+cls && echo. && echo Output filename prefix (default: %default_output_filename_prefix%) [allowed: alphanumeric, underline, hyphen and dot]: %output_filename_prefix% && echo Output file extension (default: mp4) [allowed: mp4, mkv, webm]: %output_extension%"
+set /p "audio_bitrate=Output filename audio bitrate (default: "%default_output_filename_prefix%") [allowed: numeric]: "
+if "%audio_bitrate%"=="" (
+    set "audio_bitrate=%audio_bitrate%" && goto start_transcoding_task
 )
 
 :start_transcoding_task
@@ -59,11 +67,11 @@ echo [info] File extension: "%output_extension%"
 echo [info] Video codec: "libsvtav1"
 echo [info] Video bitrate: (same as input)
 echo [info] Audio codec: "%audio_codec%"
-echo [info] Audio bitrate: "%audio_bitrate%kbps"
+echo [info] Audio bitrate: "%audio_bitrate%" (kbps)
 echo.
 
 rem Runs transcoding using the chosen AV1 codec for video and the given settings for audio
-"ffmpeg.exe" -i "%input_file_path%" -c:v %video_codec% -c:a %audio_codec% -b:a %audio_bitrate%k -y "%output_filepath%" -hide_banner -stats -loglevel warning
+"ffmpeg.exe" -i "%input_file_path%" -c:v %video_codec% -c:a %audio_codec% -b:a %audio_bitrate%k -q:a 0 -y "%output_filepath%" -hide_banner -stats -loglevel warning
 if errorlevel 0 (
     echo. && echo [success] File transcoding completed: "%output_filepath%" && "nircmd.exe" moverecyclebin "%input_file_path%" && goto endapp
 ) else (
