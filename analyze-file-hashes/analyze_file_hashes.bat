b57a0b906ac671ac19957ed914c93012eef73a37
Henrique <henriquemoreira10fk@gmail.com>
2024-06-26 19:52:07 -0300

    Added verification for input argument

--- analyze-file-hashes/analyze_file_hashes.bat
+++ analyze-file-hashes/analyze_file_hashes.bat
@@ -5,12 +5,25 @@ setlocal EnableDelayedExpansion
 rem Set terminal title
 title Analyze file hashes - Developed at gh@Henrique-Coder/windows-sendto-tools
 
+rem Check if a file is provided as argument
+if "%~1"=="" (
+    echo No file specified. Usage: analyze_hashes.bat [file_path]
+    pause >nul
+    exit /b 1
+)
+
 rem Set input file path
 set "input_file_path=%~dpnx1"
 
+rem Check if the file exists
+if not exist "!input_file_path!" (
+    echo File "%~nx1" not found.
+    pause >nul
+    exit /b 1
+)
+
 rem Calculate hashes and show them
-echo.
-echo Hashes for file: "%~dpnx1"
+echo. && echo Hashes for file: "%~dpnx1"
 set "count=0"
 for %%A in (MD5 SHA1 SHA256 SHA384 SHA512) do (
     set /a "count+=1"
